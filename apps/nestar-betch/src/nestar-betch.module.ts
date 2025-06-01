2db7089d21708a5f0eb399de120f7d2c87eba4b7
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-06-01 14:46:13 +0900

    fix: installation packages $ modify format standards

--- apps/nestar-betch/src/nestar-betch.module.ts
+++ apps/nestar-betch/src/nestar-betch.module.ts
@@ -3,8 +3,8 @@ import { NestarBetchController } from './nestar-betch.controller';
 import { NestarBetchService } from './nestar-betch.service';
 import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [NestarBetchController],
-  providers: [NestarBetchService],
+	imports: [ConfigModule.forRoot()],
+	controllers: [NestarBetchController],
+	providers: [NestarBetchService],
 })
 export class NestarBetchModule {}
