233a499d5c5e823f35d0252f3ee4422159741eef
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-06-01 14:34:22 +0900

    feat: configure env variables

--- apps/nestar-betch/src/main.ts
+++ apps/nestar-betch/src/main.ts
@@ -3,6 +3,6 @@ import { NestarBetchModule } from './nestar-betch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NestarBetchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
--- apps/nestar-betch/src/nestar-betch.module.ts
+++ apps/nestar-betch/src/nestar-betch.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { NestarBetchController } from './nestar-betch.controller';
 import { NestarBetchService } from './nestar-betch.service';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [NestarBetchController],
   providers: [NestarBetchService],
 })
