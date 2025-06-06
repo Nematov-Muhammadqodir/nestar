bc589058a5fb42d6023ce1e071bb648464d58669
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-06-06 21:10:51 +0900

    MITASK ZM DONE

--- apps/nestar-api/src/train.ts
+++ apps/nestar-api/src/train.ts
@@ -1,3 +1,20 @@
+// TASK ZM:
+
+// Shunday function yozing, va bu function parametr
+// sifatida raqamlarni qabul qilsin. Bu function qabul qilingan
+// raqamlarni orqasiga o'girib qaytarsin
+
+// MASALAN: reverseInteger(123456789); return 987654321;
+
+// Yuqoridagi misolda, function kiritilgan raqamlarni orqasiga
+// o'girib (reverse) qilib qaytarmoqda.
+
+function reverseInteger(input: number) {
+	const string = Number([...String(input)].reverse().join(''));
+	return string;
+}
+console.log(reverseInteger(123456789));
+
 // TASK ZK:
 
 // Shunday function yozing, bu function har bir soniyada bir marotaba
@@ -5,15 +22,15 @@
 // 5 soniyadan so'ng function o'z ishini to'xtatsin
 
 // MASALAN: printNumbers();
-function printNumbers() {
-	let count = 1;
-	console.log(count++);
-	const logEverySec = setInterval(() => {
-		console.log(count++);
-	}, 1000);
+// function printNumbers() {
+// 	let count = 1;
+// 	console.log(count++);
+// 	const logEverySec = setInterval(() => {
+// 		console.log(count++);
+// 	}, 1000);
 
-	setTimeout(() => {
-		clearInterval(logEverySec);
-	}, 5000);
-}
-printNumbers();
+// 	setTimeout(() => {
+// 		clearInterval(logEverySec);
+// 	}, 5000);
+// }
+// printNumbers();
