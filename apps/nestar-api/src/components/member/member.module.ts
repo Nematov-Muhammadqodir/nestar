172d82b88bd218bd472af03a264ec5c58f7a3724
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-06-11 15:44:37 +0900

    feat: hashing password via bcryptjs

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -4,9 +4,11 @@ import { MemberResolver } from './member.resolver';
 import { DatabaseModule } from '../../database/database.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
+import { AuthService } from '../auth/auth.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }])],
+	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]), AuthModule],
 
 	providers: [MemberService, MemberResolver],
 })
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -5,15 +5,19 @@ import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class MemberService {
-	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+	constructor(
+		@InjectModel('Member') private readonly memberModel: Model<Member>,
+		private authService: AuthService,
+	) {}
 
 	public async signup(input: MemberInput): Promise<Member> {
+		input.memberPassword = await this.authService.hashPassword(input.memberPassword);
 		try {
-			//TODO: Hash password
-			const result = await this.memberModel.create(input);
+			const result: Member = await this.memberModel.create(input);
 			//TODO: Authentication with tokens
 			return result;
 		} catch (err) {
@@ -32,7 +36,8 @@ export class MemberService {
 		}
 
 		//TODO: Compare passwords
-		const isMatch = memberPassword === response.memberPassword;
+		const isMatch = await this.authService.comparePassword(memberPassword, response.memberPassword);
+
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
 
 		return response;
