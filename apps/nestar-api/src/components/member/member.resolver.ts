55902444bd6ad6d59efa285320dd82608d90b8c4
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-06-16 21:56:20 +0900

    fix: modify updateMember GraphQL API

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -9,6 +9,7 @@ import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Resolver()
 export class MemberResolver {
@@ -29,20 +30,6 @@ export class MemberResolver {
 	}
 
 	@UseGuards(AuthGuard)
-	@Mutation(() => String)
-	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
-		console.log('Mutation updateMember');
-		console.log('data', typeof memberId);
-		return this.memberService.updateMember();
-	}
-
-	@Query(() => String)
-	public async getMember(): Promise<string> {
-		console.log('Mutation getMember');
-		return this.memberService.getMember();
-	}
-
-	@UseGuards(AuthGuard)
 	@Query(() => String)
 	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
 		console.log('Query: checkAuth');
@@ -60,12 +47,30 @@ export class MemberResolver {
 		return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
 	}
 
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async updateMember(
+		@Args('input') input: MemberUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation updateMember');
+		delete input._id;
+		console.log('data', typeof memberId);
+		return await this.memberService.updateMember(memberId, input);
+	}
+
+	@Query(() => String)
+	public async getMember(): Promise<string> {
+		console.log('Mutation getMember');
+		return this.memberService.getMember();
+	}
+
 	// ADMIN
 
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
 	@Mutation(() => String)
-	public async getAllMembersByAdmin(ss): Promise<string> {
+	public async getAllMembersByAdmin(): Promise<string> {
 		return this.memberService.getAllMembersByAdmin();
 	}
 
